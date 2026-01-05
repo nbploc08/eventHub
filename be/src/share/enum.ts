3820379889568184ff496571103228a3c277a875
Locvpo1233211 <trongnghia1112223333>
2026-01-05 20:43:50 +0700

    first commit

---
+++ be/src/share/enum.ts
@@ -0,0 +1,18 @@
+// User role
+export enum UserRole {
+
+}
+
+// Transaction type
+export enum TransactionType {
+
+}
+
+// Order status
+export enum OrderStatus {
+
+}
+// Payment status
+export enum PaymentStatus {
+
+}
